f296e2fcc73a01d20f0aa50b1160ea42cc13ec43
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-02-23 16:36:57 +0100

    added migration and seeder

---
+++ config/comics.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    'menu' => [
+        'characters',
+        'comics',
+        'characters',
+        'movies',
+        'tv',
+        'games',
+        'collectibles',
+        'videos',
+        'fans',
+        'news',
+        'shop'
+    ],
+    'images' => [
+        [
+          'label' => 'digital comics',
+          'file' => 'buy-comics-digital-comics.png'
+        ],
+        [
+          'label' => 'dc merchandise',
+          'file' => 'buy-comics-merchandise.png'
+        ] ,
+        [
+          'label' => 'subscription',
+          'file' => 'buy-comics-subscriptions.png'
+        ],
+        [
+          'label' => 'comic shop locator',
+          'file' => 'buy-comics-shop-locator.png'
+        ],
+        [
+          'label' => 'dc power visa',
+          'file' => 'buy-dc-power-visa.svg'
+        ]
+    ],
+    'socials' => [
+        'facebook',
+        'twitter',
+        'youtube',
+        'pinterest',
+        'periscope'
+     ]
+
+];
+
